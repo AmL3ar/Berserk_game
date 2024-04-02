05c67f0e58e4bc20f9b168c0be6f593ef2eaace4
AmLear <artem.zverev.03@bk.ru>
2024-04-03 00:32:30 +0500

    Update 1.3
    -Добавлена музыка
    -Добавлена простая настройка музыки
    -Изменены кнопки

--- Scripts/Menu_animation.gd
+++ Scripts/Menu_animation.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var animation = $AnimatedSprite2D
+@onready var audio = $Music_settings
 
 func _ready():
 	animation.play("Menu")	
@@ -8,6 +9,7 @@ func _ready():
 
 
 func _process(delta):
+	update_volume(-audio.value)
 	if animation.is_playing() == false:
 		get_tree().change_scene_to_file("res://Scenes/Story.tscn")
 
@@ -21,5 +23,9 @@ func _on_start_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+	
+	
+func update_volume(vol):
+	AudioServer.set_bus_volume_db(0, vol)
 
 
