b73c4e5ed3780fe478c4921e5c3aca32f7e13404
AmLear <artem.zverev.03@bk.ru>
2024-04-08 15:51:27 +0500

    Update 1.5
    -Улучшена логика персонажа
    -Добавлено больше анимаций персонажа

--- Scripts/Hero.gd
+++ Scripts/Hero.gd
@@ -2,29 +2,62 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 @onready var animation = $Sprite2D
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _process(delta):
-	animation.play("IdleRight")
+var last_key_pressed: String = "IdleRight"
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left","ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func controller(delta):
+	var vel = velocity
+	
+	if (!is_on_floor()):
+		vel.y += gravity * delta
+		
+	if (Input.is_action_just_pressed("ui_accept") && is_on_floor()):
+		vel.y = JUMP_VELOCITY
+		
+	if (Input.is_action_pressed("ui_right")):
+		vel.x = SPEED
+	if (Input.is_action_pressed("ui_left")):
+		vel.x = -SPEED
+		
+	if (Input.is_action_just_released("ui_right") || Input.is_action_just_released("ui_left")):
+		vel.x = 0
+			
+		
+	velocity = vel
+		
+	updateSpriteRenderer(vel.x,vel.y)	
 	move_and_slide()
+	
+func updateSpriteRenderer(velX:float,velY:float):
+	var running = velX != 0
+	var jumping = velY != 0
+	
+	if (running && !jumping):
+		animation.play("RunRight")
+		last_key_pressed = "IdleRight"
+		if velX < 0:
+			animation.flip_h = true
+			last_key_pressed = "IdleLeft"
+	elif (jumping):
+		if velY < 300:
+			animation.play("JumpRightUp")
+		else:
+			animation.play("JumpRightDown")
+		if velX < 0 || last_key_pressed == "IdleLeft":
+			if velY < 300:
+				animation.play("JumpLeftUp")
+			else:
+				animation.play("JumpLeftDown")
+		animation.stop()
+	else:
+		animation.play(last_key_pressed)
+	
+		
+func _physics_process(delta):
+	controller(delta)
+
+	
